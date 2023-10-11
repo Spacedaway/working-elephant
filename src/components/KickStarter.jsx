dd170859ad5fef0789df1bdba5c76b2f9687a338
Spacedaway <cancokeforreal@yahoo.com>
2023-10-11 21:31:38 +0100

    Added KickStarter

--- src/components/KickStarter.jsx
+++ src/components/KickStarter.jsx
@@ -5,9 +5,9 @@ import image2 from '../assets/h3-img-6.png';
 
 function KickStarter() {
 	return (
-		<div className='kickstart | even-columns container | border-radius-2'>
-			<img src={image1} alt='' />
-			<img src={image2} alt='' />
+		<div className='kickstart | even-columns container | margin-block-end-5 box-shadow-5'>
+			<img src={image1} alt='' className='kickstart--img-1'/>
+			<img src={image2} alt='' className='kickstart--img-2'/>
 		</div>
 	);
 }
