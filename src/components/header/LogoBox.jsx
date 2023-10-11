7f58ad16d89a40e9fe2d3a5ed21d502515037a5a
Spacedaway <cancokeforreal@yahoo.com>
2023-10-11 10:07:24 +0100

    Fixed Paths

--- src/components/header/LogoBox.jsx
+++ src/components/header/LogoBox.jsx
@@ -1,5 +1,5 @@
-import logoWHITE from '../../../public/assets/logo-WHITE.png';
-import logoBLUE from '../../../public/assets/logo-Blue.png';
+import logoWHITE from '/public/assets/logo-WHITE.png';
+import logoBLUE from '/public/assets/logo-Blue.png';
 
 const LogoBox = () => {
 	return (
